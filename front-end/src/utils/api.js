9be67bbcb832f0b3145260efc1d3037986ac6d6c
Mason Cole <cole.mason2@gmail.com>
2023-07-03 18:41:44 -0500

    api change

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,7 +5,7 @@
 import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
+const API_BASE_URL = "https://restaruant-reservation-1.onrender.com"
 
 /**
  * Defines the default headers for these functions to work with `json-server`
