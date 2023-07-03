52fe0b1546ee2e054538a555bc07a5bccc28535c
Mason Cole <cole.mason2@gmail.com>
2023-07-03 17:41:40 -0500

    backend changes

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -1,50 +1,17 @@
-const knex = require("../db/connection");
+const router = require("express").Router();
+const controller = require("./tables.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-const list = () => {
-  return knex("tables").select("*").orderBy("table_name");
-};
+router
+  .route("/:table_id/seat")
+  .put(controller.seatReservation)
+  .delete(controller.finishReservation)
+  .all(methodNotAllowed);
 
-const create = (newTable) => {
-  return knex("tables")
-    .insert(newTable)
-    .returning("*")
-    .then((createdRecords) => createdRecords[0]);
-};
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
 
-const read = (table_id) => {
-  return knex("tables").select("*").where({ table_id: table_id }).first();
-};
-
-// Read reservation by id in order to update a table //
-const readReservation = (reservation_id) => {
-  return knex("reservations")
-    .select("*")
-    .where({ reservation_id: reservation_id })
-    .first();
-};
-
-const update = async (updatedTable, updatedReservation) => {
-  const trx = await knex.transaction();
-  return trx("tables")
-    .select("*")
-    .where({ table_id: updatedTable.table_id })
-    .update(updatedTable, "*")
-    .then((updatedRecords) => updatedRecords[0])
-    .then(() => {
-      return trx("reservations")
-        .select("*")
-        .where({ reservation_id: updatedReservation.reservation_id })
-        .update(updatedReservation, "*")
-        .then((updatedResRecords) => updatedResRecords[0]);
-    })
-    .then(trx.commit)
-    .catch(trx.rollback);
-};
-
-module.exports = {
-  create,
-  list,
-  read,
-  readReservation,
-  update,
-};
+module.exports = router;
