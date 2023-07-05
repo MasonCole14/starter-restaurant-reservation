0ac5d6c263d47bb4af4c43dcca0e2432cbef885c
Mason Cole <cole.mason2@gmail.com>
2023-07-05 16:08:26 -0500

    fin btn

--- front-end/src/tables/TablesList.js
+++ front-end/src/tables/TablesList.js
@@ -16,7 +16,7 @@ export default function TableList({ table, setTablesError }) {
     ) {
       try {
         await finishTable(table.table_id, abortController.signal);
-        history.go(0);
+        history.goBack();
       } catch (error) {
         setTablesError([error.message]);
       }
